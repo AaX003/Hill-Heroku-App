57e9bbf676493fe1ebcfb8573e51781d26a8201b
aaxhil <aaxhil8969.students.ecpi.edu>
2025-03-31 20:03:02 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>My Heroku App</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
